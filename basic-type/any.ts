0bb5873c80219a51a0a83e603c0fb233b71cfa62
Jongkeun Kim <kjkandrea@gmail.com>
2020-08-30 14:50:05 +0900

    basic type : any

---
+++ basic-type/any.ts
@@ -0,0 +1,9 @@
+let basket:any
+basket = "hello, any!"
+console.log("1. say hello : %s", basket)
+
+let basketArray:any[] = [1, true, "string"]
+console.log("2. any array print : %s", basketArray)
+
+let number:any = 50
+console.log("3. to Fixed any(number)", number.toFixed(2))
