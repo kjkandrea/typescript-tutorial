4db88c0b19c3f11abb44de7e52a2add4bc68656f
Jongkeun Kim <kjkandrea@gmail.com>
2020-08-31 18:26:01 +0900

    bypass tslint conflict

--- basic-type/typeError.ts
+++ basic-type/typeError.ts
@@ -1,4 +1,4 @@
-const str:string = "100"
+const strr:string = "100"
 
 // console.log(str * 1, typeof str * 1) // type error
-console.log(Number(str), typeof Number(str))
+console.log(Number(strr), typeof Number(strr))
